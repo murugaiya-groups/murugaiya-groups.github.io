d9d9d6fd0b58edc30a345435f2b51e63a79262e9
Murugesan <murugesan.narayan@gmail.com>
2021-12-05 08:15:42 +0100

    added kadavul

--- kambaramayanam/index.js
+++ kambaramayanam/index.js
@@ -14,7 +14,7 @@ function getContent(nav, name) {
   var elmnt = document.getElementById('mid-content')
   var xhttp = new XMLHttpRequest()
   xhttp.onreadystatechange = function () {
-    if (this.readyState == 4) {
+    if (this.readyState == XMLHttpRequest.DONE) {
       if (this.status == 200) {
         elmnt.innerHTML = this.responseText
       }
